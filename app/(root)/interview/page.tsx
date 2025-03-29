5c1e13fbdecec9d10cb3e107412c0b9c72368467
Muhammad Putra Age <muhammadputraage@gmail.com>
2025-03-29 12:15:33 +0800

    implement interview generation page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,13 @@
+import Agent from "@/components/Agent";
+import React from "react";
+
+const Interview = () => {
+  return (
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName="You" type="generate" />
+    </>
+  );
+};
+
+export default Interview;
